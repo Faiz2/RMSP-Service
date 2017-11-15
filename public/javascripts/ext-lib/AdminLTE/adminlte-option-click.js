3ae3ff4372e3a7c07d538e458d32207c727d360d
钱鹏 <891366402@qq.com>
2017-11-15 15:12:46 +0800

    小修改

--- public/javascripts/ext-lib/AdminLTE/adminlte-option-click.js
+++ public/javascripts/ext-lib/AdminLTE/adminlte-option-click.js
@@ -1,10 +1,40 @@
 (function($, w){
-    $('ul[class="sidebar-menu"] li').click(function(evt){
-        // var li = evt
+    $('ul[class="sidebar-menu"] li a').click(function(){
+        replace_style($(this));
+        var li_filter = $(this).attr('pharbers-filter');
+        switch (li_filter) {
+            case 'market-info':
+                replace_page("test");
+                break;
+            case 'brd':
+                break;
+            case 'product-info':
+                break;
+            case 'business-decisions':
+                break;
+            case 'management-decisions':
+                break;
+            case 'report':
+                break;
+            case 'help':
+                break;
+            default:
+        }
 
-        // $.get("/test", {}, function(r){
-        //     console.info(r);
-        // });
-        console.info(evt);
+
     });
+
+    var replace_page = function(link) {
+        $.get("/" + link, {}, function(r){
+            var $content = $('div[pharbers-filter="div-content"]');
+            $content.empty();
+            $content.html(r);
+        });
+    }
+    var replace_style = function (doc) {
+        var cur_li = doc.parent().not('.treeview');
+        var li = $('.sidebar-menu').find('li').not('.header').not('.treeview');
+        li.attr('class', '');
+        cur_li.attr('class', 'active');
+    }
 })(jQuery, window);
