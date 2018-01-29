63d2900491fb18a44f6af50b6441fbca7e20edc0
AlfredYang1986 <yangyuanpig@gmail.com>
2018-01-29 15:44:10 +0800

    report 框架，以后修改

--- app/controllers/RMSPRoutesController.scala
+++ app/controllers/RMSPRoutesController.scala
@@ -208,15 +208,9 @@ class RMSPRoutesController @Inject()(as_inject: ActorSystem, dbt: dbInstanceMana
         }
     }
 
-//	def businessDecision = Action { request =>
-//		getUserCookie(request)(Ok(views.html.Module.Decision.BusinessDecision.index()))
-//	}
-//
-//	def managementDecision = Action { request =>
-//		getUserCookie(request)(Ok(views.html.Module.Decision.ManagementDecision.index()))
-//	}
-
 	def report = Action { request =>
-		getUserCookie(request)(Ok(views.html.Module.Report.index()))
+        getUserCookie(request) {
+            Ok(views.html.Module.Report.report_index())
+        }
 	}
 }
