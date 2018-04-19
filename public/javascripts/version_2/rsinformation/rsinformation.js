2e31f99cd0fa0c281dc7354f96386d522df61759
Faiz <891366402@qq.com>
2018-04-19 19:18:42 +0800

    新增雷达图

--- public/javascripts/version_2/rsinformation/rsinformation.js
+++ public/javascripts/version_2/rsinformation/rsinformation.js
@@ -3,7 +3,6 @@
     var overflowTableContainerEle = overflowTableContainer[0];
     var tableHeade = $(".overflow-table-head");
     var table = $(".overflow-table");
-    var order = 0;
     var tablelen = table.length;
     var oldTableWidth = 0;
     var scrollBarLength = 0;
@@ -80,8 +79,137 @@
         }
     };
 
+    var personRadarMap = function(identify) {
+        var radarmap = echarts.init($('.person-list div[name="'+ identify +'"]')[0]);
+        var lineStyle = {
+            normal: {
+                width: 2,
+                opacity: 0.9
+            }
+        };
+
+        var option = {
+            tooltip: {
+                confine: true
+            },
+            title: {},
+            legend: {
+                top: 'top',
+                left: 'right',
+                orient: 'vertical',
+                data: ['当期', '上期'],
+                itemGap: 20,
+                icon:'circle',
+                textStyle: {
+                    color: '#fff',
+                    fontSize: 10
+                },
+                // selectedMode: 'single'
+            },
+            radar: {
+                indicator: [
+                    {name: '销售技巧', max: 100},
+                    {name: '产品知识', max: 100},
+                    {name: '工作积极性', max: 100}
+                ],
+                center: ['48%', '55%'],
+                radius: 75,
+                shape: 'circle',
+                splitNumber: 5,
+                name: {
+                    textStyle: {
+                        fontSize: 10,
+                        color: 'rgb(236, 236, 236)'
+                    }
+                },
+                splitLine: {
+                    lineStyle: {
+                        color: [
+                            new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                "offset": 0,
+                                "color": '#2496dc',
+
+                            }, {
+                                "offset": 1,
+                                "color": '#FE0059'
+                            }]),
+
+                            new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                "offset": 0,
+                                "color": '#FE601B',
+
+                            }, {
+                                "offset": 1,
+                                "color": '#FE0059'
+                            }]),
+
+                            new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                "offset": 0,
+                                "color": '#FE601B',
+
+                            }, {
+                                "offset": 1,
+                                "color": '#00E8F8'
+                            }])
+
+                        ]
+                        // color: [
+                        //     'rgba(238, 197, 102, 0.1)', 'rgba(238, 197, 102, 0.2)',
+                        //     'rgba(238, 197, 102, 0.4)', 'rgba(238, 197, 102, 0.6)',
+                        //     'rgba(238, 197, 102, 0.8)', 'rgba(238, 197, 102, 1)'
+                        // ].reverse()
+                    }
+                },
+                splitArea: {
+                    show: false
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: 'rgba(238, 197, 102, 0.5)'
+                    }
+                }
+            },
+            series: [
+                {
+                    name: '人物评分',
+                    type: 'radar',
+                    symbol: 'none',
+                    lineStyle: lineStyle,
+                    areaStyle: {normal: {}},
+                    data : [
+                        {
+                            value : [40, 90, 50 ],
+                            name : '当期',
+                            itemStyle: {
+                                normal: {
+                                    color: '#0E86A4'
+                                }
+                            }
+                        },
+                        {
+                            value : [80, 50, 12],
+                            name : '上期',
+                            itemStyle: {
+                                normal: {
+                                    color: '#A6A921'
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        };
+        radarmap.setOption(option);
+    };
+
     $(function(){
 
+        init: {
+            var person = ["小宋", "小兰", "小木", "小白", "小青"];
+            $.each(person, function(i, v){
+                personRadarMap(v)
+            });
+        }
         events: {
             //资源页面 收起按钮
             $('#backup-btn').click(function() {
